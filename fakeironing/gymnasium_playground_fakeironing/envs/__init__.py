868fc276688d17d961c389c5834b30b651a6af0c
jgvictores <jcgvicto@gmail.com>
2023-03-28 14:59:44 +0200

    fix init

--- fakeironing/gymnasium_playground_fakeironing/envs/__init__.py
+++ fakeironing/gymnasium_playground_fakeironing/envs/__init__.py
@@ -1,6 +1,1 @@
-from gymnasium.envs.registration import register
-
-register(
-    id="gymnasium_playground/FakeIroning-v0",
-    entry_point="gymnasium_playground_fakeironing.envs:FakeIroningEnv",
-)
+from gymnasium_playground_fakeironing.envs.fake_ironing import FakeIroningEnv
